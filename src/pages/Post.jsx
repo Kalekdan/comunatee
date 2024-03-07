ec48d44932281293c92fac7cd84c87fdbc2cf6d7
Kalekdan <joerickard@hotmail.co.uk>
2024-03-07 09:06:32 +0000

    Removed unused params

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -3,7 +3,8 @@ import { useParams } from "react-router-dom";
 import posts from "../data_models/posts.json";
 import CommentsSection from "../components/CommentsSection";
 const Post = () => {
-  const { comunatee, username, postId } = useParams();
+  // const { comunatee, username, postId } = useParams();
+  const { postId } = useParams();
 
   return <div>
     {posts.filter(x=>x.id === postId)[0].content}
